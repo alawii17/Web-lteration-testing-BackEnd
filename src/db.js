c04d631eeee7bcd0a362650fbd91c4116c24b169
alawii17 <alawialatas1414@gmail.com>
2024-06-18 00:43:03 +0700

    Konfigurasi railway

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'literation_testing',
